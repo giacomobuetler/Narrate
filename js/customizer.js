5b50db12219c52f6589a8acbfc8f224b5261778c
giacomonarrate <giacomo.buetler@narrate.ch>
2021-01-20 23:29:05 +0100

    Header Hover

--- js/customizer.js
+++ js/customizer.js
@@ -44,10 +44,3 @@
 /**
  * Custom navigation toggler for activating dropdowns on hover instead of on click
  */
-$('body').on('mouseenter mouseleave','.dropdown',function(e){
-	var _d=$(e.target).closest('.dropdown');_d.addClass('show');
-	setTimeout(function(){
-	  _d[_d.is(':hover')?'addClass':'removeClass']('show');
-	  $('[data-toggle="dropdown"]', _d).attr('aria-expanded',_d.is(':hover'));
-	},300);
-  });
