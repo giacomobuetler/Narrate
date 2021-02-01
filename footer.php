c1fe51384dafd3f3eef30aeebd4674419eb033aa
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-01 04:27:08 +0100

    Update Footer

--- footer.php
+++ footer.php
@@ -22,7 +22,14 @@ $container = get_theme_mod( 'understrap_container_type' );
                     <div class="col-lg-3 col-md-4">
                         <a class="footer-logo d-block" href="https://codely.ch">Codely</a>
                         <p class="footer-text mb-0">Hochwertige Webs
-							High-quality digital resources which make your design workflow faster and easier</p>
+							High-quality digital resources which make your design workflow faster and easier</p><br>
+							Codely GmbH<br>
+							Farbgutstrasse 3<br>
+							CH-9008 St.Gallen<br>
+							Schweiz<br>
+							<br>
+							<a href="tel:+41 71 571 37 22">+41 71 571 37 22</a><br>
+							<a href="mailto:info@codely.ch">info@codely.ch</a>
                     </div>
                     <div class="col-lg-9 col-md-7">
 				        <div class="row">
