6b40ef614c5904b66fceef5aef0feaff15f09fcf
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-07 02:10:11 +0100

    Update Header

--- header.php
+++ header.php
@@ -25,14 +25,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 <script>
-$(document).ready(function(){
+/* $(document).ready(function(){
     $(".dropdown").hover(function(){
         var dropdownMenu = $(this).children(".dropdown-menu");
         if(dropdownMenu.is(":visible")){
             dropdownMenu.parent().toggleClass("open");
         }
     });
-});     
+});     */ 
 </script>
 <div id="wrapper-navbar sticky-top">
 
