3c7f724d72b10eb3e26972525524f422d31ae713
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-07 02:25:22 +0100

    Reset Header

--- header.php
+++ header.php
@@ -19,44 +19,38 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
-
-	<script src="https://kit.fontawesome.com/bc90823a43.js" crossorigin="anonymous"></script>
-
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
-<script>
-$(document).ready(function(){
-    $(".dropdown").hover(function(){
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
-        }
-    });
-});     
-</script>
-<div id="wrapper-navbar sticky-top">
 
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
 
-		<header id="header" class="n-header">
+		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
+		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
+			<h2 id="main-nav-label" class="sr-only">
+				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+			</h2>
 
-		<div class="container">
-		<div class="row justify-content-between align-items-center flex-nowrap">
+		<?php if ( 'container' === $container ) : ?>
+			<div class="container">
+		<?php endif; ?>
 
+					<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
 
 						<?php if ( is_front_page() && is_home() ) : ?>
-							<div class="col-auto">
+
 							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-							</div>	
+
 						<?php else : ?>
-							<div class="col-auto">
+
 							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-							</div>
+
 						<?php endif; ?>
 
 						<?php
@@ -64,11 +58,13 @@ $(document).ready(function(){
 						the_custom_logo();
 					}
 					?>
+					<!-- end custom logo -->
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
+				<!-- The WordPress Menu goes here -->
 				<?php
 				wp_nav_menu(
 					array(
@@ -83,9 +79,10 @@ $(document).ready(function(){
 					)
 				);
 				?>
-</div>
 			<?php if ( 'container' === $container ) : ?>
-			</div>
+			</div><!-- .container -->
 			<?php endif; ?>
-</header>
-</div>
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
