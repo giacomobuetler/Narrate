e2c0fb085cd8afdee3c4c84f46f391f1dbdbcad4
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-07 02:31:56 +0100

    Update Header

--- header.php
+++ header.php
@@ -36,28 +36,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 			</h2>
 
-		<?php if ( 'container' === $container ) : ?>
 			<div class="container">
-		<?php endif; ?>
 
 					<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
 
 						<?php if ( is_front_page() && is_home() ) : ?>
-
+							<div class="col-auto">
 							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
+							</div>	
 						<?php else : ?>
-
+							<div class="col-auto">
 							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
+							</div>
 						<?php endif; ?>
 
 						<?php
-					} else {
+						} else {
 						the_custom_logo();
-					}
-					?>
+						}
+						?>
 					<!-- end custom logo -->
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
