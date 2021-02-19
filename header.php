ad944dd7e26cb5ff6a291397d4d7949fa8cf9b84
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-19 17:53:12 +0100

    Update Header

--- header.php
+++ header.php
@@ -47,11 +47,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 						<?php if ( is_front_page() && is_home() ) : ?>
 							<div class="col-auto">
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?><img src="https://codely.ch/app/wp-content/uploads/2021/02/codely-w.svg" loading="lazy" alt="" class="logo-codely"></a></h1>
 							</div>	
 						<?php else : ?>
 							<div class="col-auto">
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?><img src="https://codely.ch/app/wp-content/uploads/2021/02/codely-w.svg" loading="lazy" alt="" class="logo-codely"></a>
 							</div>
 						<?php endif; ?>
 
