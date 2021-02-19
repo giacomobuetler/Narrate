3001d40237ae253290ca631a8e1478a9f1f3eef7
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-19 18:20:58 +0100

    Update

--- header.php
+++ header.php
@@ -42,27 +42,25 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 
 					<!-- Your site title as branding in the menu -->
+					<!-- Your site title as branding in the menu -->
+					<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
-							<div class="col-auto">
-							<a class="navbar-brand mb-0">
-								<img src="https://codely.ch/app/wp-content/uploads/2021/02/codely.svg" alt="" class="logo-codely">
-								</a>
-							</div>	
-						<?php else : ?>
-							<div class="col-auto">
-							<a class="navbar-brand mb-0">
-								<img src="https://codely.ch/app/wp-content/uploads/2021/02/codely.svg" alt="" class="logo-codely">
-								</a>							</div>
-						<?php endif; ?>
+<?php if ( is_front_page() && is_home() ) : ?>
 
-						<?php
-						} else {
-						the_custom_logo();
-						}
-						?>
-					<!-- end custom logo -->
+	<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+<?php else : ?>
+
+	<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+<?php endif; ?>
+
+<?php
+} else {
+the_custom_logo();
+}
+?>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
