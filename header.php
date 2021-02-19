bdb639ce1e984220e90deb23deb72cce13a18d87
giacomonarrate <giacomo.buetler@narrate.ch>
2021-02-19 18:27:50 +0100

    Update Header

--- header.php
+++ header.php
@@ -39,28 +39,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</h2>
 
 			<div class="container">
-
-
-					<!-- Your site title as branding in the menu -->
-					<!-- Your site title as branding in the menu -->
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
-
-<?php if ( is_front_page() && is_home() ) : ?>
-
-	<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-<?php else : ?>
-
-	<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-<?php endif; ?>
-
-<?php
-} else {
-the_custom_logo();
-}
-?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img src="https://codely.ch/app/wp-content/uploads/2021/02/codely.svg" loading="lazy" alt="" class="logo-codely">
+				</a>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
