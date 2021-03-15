c592d75150498e30092edf5b9e0fb68fb3387176
giacomonarrate <giacomo.buetler@narrate.ch>
2021-03-16 00:37:06 +0100

    Update Header

--- header.php
+++ header.php
@@ -59,7 +59,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</h2>
 
 			<div class="container">
-				<div class="row">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 					<img src="https://codely.ch/app/wp-content/uploads/2021/02/codely.svg" loading="lazy" alt="" class="logo">
 				</a>
@@ -85,7 +84,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 				?>
 
 			<?php if ( 'container' === $container ) : ?>
-			</div>
 			</div>
 			</div><!-- .container -->
 			<?php endif; ?>
