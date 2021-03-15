062ddca8148a818fe8fdf44fb1a9f4b432f5c921
giacomonarrate <giacomo.buetler@narrate.ch>
2021-03-16 00:29:01 +0100

    Update Header

--- header.php
+++ header.php
@@ -43,7 +43,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
@@ -52,16 +51,35 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav id="main-nav" class="navbar navbar-expand-md navbar-light" aria-labelledby="main-nav-label">
+		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
 			<h2 id="main-nav-label" class="sr-only">
 				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 			</h2>
 
+		<?php if ( 'container' === $container ) : ?>
 			<div class="container">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<img src="https://codely.ch/app/wp-content/uploads/2021/02/codely.svg" loading="lazy" alt="" class="logo">
-				</a>
+		<?php endif; ?>
+
+					<!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
+
+						<?php if ( is_front_page() && is_home() ) : ?>
+
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+						<?php else : ?>
+
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+						<?php endif; ?>
+
+						<?php
+					} else {
+						the_custom_logo();
+					}
+					?>
+					<!-- end custom logo -->
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
@@ -82,9 +100,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					)
 				);
 				?>
-
 			<?php if ( 'container' === $container ) : ?>
-			</div>
 			</div><!-- .container -->
 			<?php endif; ?>
 
